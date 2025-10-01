662928e530d668a3076483e50b9ae47f0d968efc
Jan_Reisiegel <jan.reisiegel@tul.cz>
2025-10-01 17:48:50 +0200

    CI/CD tests

---
+++ cviceni/SmartCaffe/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "2.2.10"
+    jacoco
+}
+
+group = "reisiegel.jan"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+}
+
+tasks.jacocoTestReport{
+    reports {
+        // Zajištění, že se generuje XML report, který Codecov potřebuje
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(false) // Volitelné, můžete nechat false pro úsporu místa
+    }
+}
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+kotlin {
+    jvmToolchain(19)
+}
+
