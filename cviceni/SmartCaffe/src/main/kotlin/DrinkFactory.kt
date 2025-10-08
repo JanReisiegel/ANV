7570aaa0298eab55cb121482d78e95ca8d8903c3
Jan_Reisiegel <jan.reisiegel@tul.cz>
2025-10-08 12:05:44 +0200

    Builder

--- cviceni/SmartCaffe/src/main/kotlin/DrinkFactory.kt
+++ cviceni/SmartCaffe/src/main/kotlin/DrinkFactory.kt
@@ -1,7 +1,7 @@
 import drinks.CustomDrink
 
 abstract class DrinkFactory {
-    abstract fun createDrink(type: String, milk: Boolean, sugar: Boolean, caramel: Boolean, honey: Boolean, cinnamon: Boolean): CustomDrink;
+    abstract fun createDrink(type: String, milk: Boolean, sugar: Boolean, caramel: Boolean, honey: Boolean, cinnamon: Boolean): CustomDrink
     open fun serveDrink(type: String, milk: Boolean = false, sugar: Boolean = false, caramel: Boolean = false, honey: Boolean = false, cinnamon: Boolean = false): String{
         throw NotImplementedError()
     }
